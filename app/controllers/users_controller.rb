8911b7316293667472a2aeae419648ee0fc9b0c5
Schifferking <schifferking@hotmail.com>
2023-05-15 19:15:50 -0600

    Feat: Add default photo using gravatar
    Note: Couldn't use an auto generated photo given the options in the API
    documentaion or displaying a public photo from an URL.
    
    More info here: https://en.gravatar.com/site/implement/images/

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  require 'digest/md5'
+
   def index
     @users = User.all
     @relationship = Relationship.new
@@ -7,5 +9,16 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @relationship = Relationship.new
+    @user_photo_url = compile_user_photo(@user.email.downcase)
+  end
+
+  private
+
+  def compile_user_photo(email)
+    "https://www.gravatar.com/avatar/#{create_hash(email)}"
+  end
+
+  def create_hash(email)
+    Digest::MD5.hexdigest(email)
   end
 end
